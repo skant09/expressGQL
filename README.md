0587de4eebc54f1d64329f19c3d7ca2e327c18ba
suryaZivame <surya.k@zivame.com>
2020-06-02 18:19:58 +0530

    Updating readme

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ yarn start
 
 ```
 go to localhost:5000/graphql
+
+### TODO
+
+- [ ] Add Mutation to demonstarate post/put api
+- [ ] Sequential fetch( when one data depends on another api call)
+- [ ] understand the organisation of schema and url to make it maintainable
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
-import express, { Application, Request, Response, NextFunction, response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import graphqlHTTP from 'express-graphql';
-import { buildSchema } from 'graphql';
 import schema from './schema';
 
 const app: Application = express();
@@ -11,13 +10,12 @@ var root = {
   },
 };
 
-
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true,
 }));
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => res.send("Hello"));
+app.get("/", (req: Request, res: Response, next: NextFunction) => res.send("Go to http://localhost:5000/graphql for graphql"));
 
 app.listen(5000, () => console.log("Running a GraphQL API server at http://localhost:5000/graphql"));
