f00884eeb70a39dde7b83f92f02d7baa9aa9cd5f
suryaZivame <surya.k@zivame.com>
2020-06-02 16:34:36 +0530

    product schema

--- src/schema.ts
+++ src/schema.ts
@@ -5,39 +5,67 @@ import {
     GraphQLString,
     GraphQLList,
     GraphQLID,
-    graphql
+    graphql,
+    GraphQLEnumType,
+    GraphQLBoolean,
+    GraphQLFloat
 } from 'graphql';
 import { query } from 'express';
 import { resolve } from 'dns';
+import { url } from 'inspector';
 
 const ProductType = new GraphQLObjectType({
     name: 'Product',
     description: '...',
 
     fields: () => ({
-        id: { type: GraphQLID },
-        sku: { type: GraphQLString }
+        productId: { type: GraphQLID },
+        sku: { type: GraphQLString },
+        name: { type: GraphQLString },
+        url: { type: GraphQLString },
+        brandName: { type: GraphQLString },
+        mainCategoryId: { type: GraphQLInt },
+        mainCategoryLink: { type: GraphQLString },
+        mainCategoryName: { type: GraphQLString },
+        wishlistCount: { type: GraphQLInt },
+        rating: { type: GraphQLFloat },
+        reviewCount: { type: GraphQLInt },
+        currency: {
+            type: new GraphQLEnumType({
+                name: 'currency',
+                values: {
+                    rupee: {
+                        value: '₹'
+                    }
+                }
+
+            }),
+        },
+        price: { type: GraphQLInt },
+        specialPrice: { type: GraphQLInt },
+        color: { type: GraphQLString },
+        stockStatus: { type: GraphQLBoolean },
+        offerCount: { type: GraphQLInt },
+        mchLevel3data: { type: GraphQLString },
+        discountPercent: { type: GraphQLInt },
+        brandLink: { type: GraphQLString }
     })
 })
 
 const schema = new GraphQLSchema({
     query: new GraphQLObjectType({
         name: 'query',
-        description: '....',
+        description: 'Root Query',
         fields: () => ({
             product: {
                 name: 'product',
+                description: 'Product query',
                 type: ProductType,
                 args: {
-                    id: { type: GraphQLID }
+                    productId: { type: GraphQLID }
                 },
-                async resolve(root, { args }, { fetchItem }) {
-                    if (args === "1") {
-                        return { id: 123, sku: "Zivame" }
-                    }
-                    let product = await fetchItem(144697);
-                    console.log(product);
-                    return product;
+                async resolve(root, { productId }, { fetchItem }) {
+                    return await fetchItem(productId);
                 }
             }
         })
